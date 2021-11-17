488882a1d1a62664234bde040274087b8fec7fe1
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-17 21:01:53 +0000

    add func to render all tweets in database and append to index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,19 +7,42 @@
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function() {
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  renderTweets = (data) => {
+
+    for(let val of data) {
+      let $tweet = createTweetElement(val);
+      $('.tweet-container').append($tweet);
+    }
+
   }
 
   createTweetElement = (data) => {
+    
     const $tweet = $('<article></article>');
 
     const $header = $(`<header><div id="name"><img src=${data.user["avatars"]}>${data.user["name"]}</div>
@@ -30,11 +53,11 @@ $(document).ready(function() {
     const $footer = $(`<footer><div id="time">${timeago.format(data.created_at)}</div>
                           <div class="icons"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>`);
     
-    return $tweet.append($header).append($body).append($footer);
+    $tweet.append($header).append($body).append($footer);
+
+    return $tweet;
   }
 
-  const $tweet = createTweetElement(tweetData);
-
-  $('.tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+  renderTweets(data);
+  
 });
