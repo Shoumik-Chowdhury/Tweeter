f58be6f056f04fbd6c1524493ad6a50a991a9968
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-18 21:01:32 +0000

    add functionality so tweets show up in reverse-chronological order, add slight delay between tweet post and get request, error alerts replaced with html msg

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     
     for(let val of data) {
       let $tweet = createTweetElement(val);
-      $('.tweet-container').append($tweet);
+      $('.tweet-container').prepend($tweet);
     }
     
   }
@@ -22,27 +22,52 @@ $(document).ready(function() {
     const $header = $(`<header><div id="name"><img src=${data.user["avatars"]}>${data.user["name"]}</div>
     <div id="handle">${data.user["handle"]}</div></header>`);
     
-    const $body = $(`<div class="tweet-body">${data.content["text"]}</div>`);
+    const $body = $(`<div class="tweet-body">${escape(data.content["text"])}</div>`);
     
     const $footer = $(`<footer><div id="time">${timeago.format(data.created_at)}</div>
     <div class="icons"><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></div>`);
     
     $tweet.append($header).append($body).append($footer);
-    
+
     return $tweet;
   }
   
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+
   $("form").submit(function(event) {
     
     event.preventDefault();
     
     let $formData = $(this).serialize();
 
-    if (!$('#tweet-text').val()) return alert('Cannot post empty tweet!!!');
-    if ($('.counter').val() < 0) return alert('Tweet too long!!!');
+    if (!$('#tweet-text').val()) {
+      let errMsg  = '<div id="empty-tweet">Cannot post empty tweet! That\'s illegal! <i class="fas fa-exclamation-triangle"></i></div>';
+      $(".error-text").html(errMsg).hide().slideDown();
+      return;
+    };
+    
+    if ($('.counter').val() < 0) {
+      let errMsg = '<div id="long-tweet">Cannot post more than 140 characters! <i class="fas fa-exclamation-triangle"></i></div>';
+      $(".error-text").html(errMsg).hide().slideDown();
+      return;
+    };
+    
+    $(".error-text").empty();
 
     $.post("/tweets/", $formData);
     
+    setTimeout(()=>{
+      $.get("/tweets", (res) => {
+        let lastTweet = res[res.length - 1];
+        renderTweets([lastTweet]);
+      })
+    }, 300);
+
   });
   
   loadTweets = function() {
